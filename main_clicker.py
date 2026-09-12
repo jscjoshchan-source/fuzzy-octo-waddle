f7c0d05dc50ca9d512c6ece2b1958c1a456e4cc2
Joshua Chan <jsc.joshchan@gmail.com>
2026-09-12 16:41:54 -0700

    started basic window

--- main_clicker.py
+++ main_clicker.py
@@ -1,1 +1,16 @@
-import pyautogui
+import tkinter as tk
+import pyautogui as py
+root = tk.Tk()
+
+#create main window
+root.title("Auto Clicker")
+root.geometry("100x200")
+
+#create text label
+label = tk.Label(root, text="Auto Clicker", font=("Arial", 12))
+
+#make it visible
+label.pack(pady=5)
+
+#start loop to keep window open
+root.mainloop()
